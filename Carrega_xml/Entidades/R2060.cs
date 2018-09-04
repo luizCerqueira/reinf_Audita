df675c655932191138a9c5164bb3116b518f1b25
Weslley <weslleysantos@auditaconsultoria.com.br>
2018-09-04 11:06:26 -0300

    Criação de entidades das tabelas do banco

---
+++ Carrega_xml/Entidades/R2060.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+	class R2060 : Entidade
+	{
+		public string tpAmb { get; set; }
+
+		public string procEmi { get; set; }
+
+		public string verProc { get; set; }
+
+		public string indRetif { get; set; }
+
+		public string nrRecibo { get; set; }
+
+		public DateTime perApur { get; set; }
+
+		public string tpInscContri { get; set; }
+
+		public string nrInscContri { get; set; }
+
+		public string tpInscEstab { get; set; }
+
+		public string nrInscEstab { get; set; }
+
+		public double vlrRecBrutaTotal { get; set; }
+
+		public double vlrCPApurTotal { get; set; }
+
+		public double vlrCPRBSuspTotal { get; set; }
+
+		public int tpProcesso { get; set; }
+
+		public string nrProcesso { get; set; }
+
+		public string codSusp { get; set; }
+
+		public int R1000 { get; set; }
+
+	}
+}
---
+++ Carrega_xml/Entidades/R2060infoProc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+	class R2060infoProc : Entidade
+	{
+		public string tpProc { get; set; }
+
+		public string nrProc { get; set; }
+
+		public string codSusp { get; set; }
+
+		public string vlrCPRBSusp { get; set; }
+
+		public string R2060tipoCod { get; set; }
+	}
+}
