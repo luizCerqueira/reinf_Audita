abd62fea0026c629aea8a080445c516eac147a1f
Bruno <brunopaz@auditaconsultoria.com.br>
2018-09-06 17:04:48 -0300

    Desenvolvidas as classes de entidade e de dao da tabela R1070 e suas derivadas

---
+++ Carrega_xml/DAO/DaoR1070.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace DAO
+{
+	public class DaoR1070 : IDAO<R1070>
+	{
+
+		public bool Delete(R1070 entidade, string Banco)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Save(R1070 entidade, string Banco, int Id, string Chave)
+		{
+			try
+			{
+
+				string strQuery = "INSERT INTO [dbo].[R1070]([tpAmb],[procEmi],[verProc],[indRetif],[nrRecibo],[perApur],[tpInscContri],[nrInscContri],[tpInscEstab],[nrInscEstab],[R1000],[Chave])";
+				strQuery += string.Format("VALUES ('{0}','{1}','{2: yyyy-MM-dd}','{3: yyyy-MM-dd}','{4}','{5}','{6}','{7}',{8},'{9}')",
+					entidade.tpProc,
+					entidade.nrProc,
+					entidade.iniValid,
+					entidade.fimValid,
+					entidade.indAutoria,
+					entidade.ufVara,
+					entidade.codMunic,
+					entidade.idVara,
+					Id,
+					entidade.Chave
+					);
+
+				using (ConexaoBD _BD = new ConexaoBD(Banco))
+				{
+					var Ide = _BD.InserirDado(strQuery);
+					entidade.Id = Convert.ToInt32(Ide);
+				}
+
+
+				return true;
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+
+		}
+
+		public bool Update(R1070 entidade, string Banco)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- Carrega_xml/Entidades/R1070.cs
+++ Carrega_xml/Entidades/R1070.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class R1070 : Entidade
+	public class R1070 : Entidade
 	{
-        //fazer
-    }
+		public string tpProc { get; set; }
+
+		public string nrProc { get; set; }
+
+		public DateTime iniValid { get; set; }
+
+		public DateTime fimValid { get; set; }
+
+		public string indAutoria { get; set; }
+
+		public string ufVara { get; set; }
+
+		public string codMunic { get; set; }
+
+		public string idVara { get; set; }
+
+		public int R1000 { get; set; }
+}
 }
